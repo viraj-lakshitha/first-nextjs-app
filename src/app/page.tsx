42ec2b737caf20e0c193f5309b87938531a44195
Viraj Lakshitha Bandara <virajlakshithabandara@gmail.com>
2023-11-10 16:57:38 +0530

    feat: init simple route mechanism

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-row items-center justify-center">
-      <p className='text-4xl'>Hello World</p>
-    </main>
-  )
+  return (<div className='flex items-center flex-row justify-center min-h-screen'>Home Page</div>)
 }
